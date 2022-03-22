7d77d19861c8f6526e76d3ab47117f419f82fed8
yoatif <y.retif@rns.ecolescreatives.com>
2022-03-22 10:25:15 +0100

    Ex2

--- Exercice1.cpp
+++ Exercice1.cpp
@@ -1,7 +1,7 @@
 #include <iostream> /* bibliothèque d'entrée et sortie standard*/
 #include <string>
 
-using namespace std; /* permet de remplacer l'espace de nom std et ici remplace "std::cout"" par cout<< " */
+
 
 int main () /* délimite la fonction principale */
 {
@@ -17,6 +17,6 @@ int main () /* délimite la fonction principale */
     else{
         std::cout << "vous etes arrivé au Mali." << std::endl;
     }
-
-
+    
+    return 0:
 }
---
+++ Exercice2.cpp
@@ -0,0 +1,23 @@
+#include <iostream> /* bibliothèque d'entrée et sortie standard*/
+#include <string>
+
+
+
+int main () /* délimite la fonction principale */
+{
+    char Ukraine;
+    char Mali
+
+    std::cout << "Bienvenue chez ETPA travelling." << std::endl;
+    std::cout << "les destinations actuelement en promo sont l'Ukraine ou le Mali" << std::endl;
+    std::cout << "quel destinations souhaitez vous reservé ?" << std::endl;
+    std::cin >> Ukraine;
+    if (destination== Ukraine){
+        std::cout << "vous etes arrivé en Ukraine." << std::endl;
+    }
+    else{
+        std::cout << "vous etes arrivé au Mali." << std::endl;
+    }
+
+    return 0:
+}
